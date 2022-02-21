33cc95af9a831778537df442f49f5555e802f69a
Eliot996 <math10t6@stud.kea.dk>
2022-02-21 17:02:58 +0100

    added HomeController

---
+++ src/main/java/com/example/moviefacts/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.moviefacts.controllers;
+
+import com.example.moviefacts.models.DataHolder;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    DataHolder dataHolder = new DataHolder();
+
+
+}
