e94a08f20e9349606c4dcd3be6263e41a0aa7b0b
Eliot996 <math10t6@stud.kea.dk>
2022-02-21 17:02:32 +0100

    added MovieFacts

---
+++ src/main/java/com/example/moviefacts/models/MovieFacts.java
@@ -0,0 +1,43 @@
+package com.example.moviefacts.models;
+
+public class MovieFacts {
+    private int Year;
+    private int Length;
+    private String Title;
+    private String Subject;
+    private int Popularity;
+    private boolean Awards;
+
+    public MovieFacts(int year, int length, String title, String subject, int popularity, boolean awards) {
+        Year = year;
+        Length = length;
+        Title = title;
+        Subject = subject;
+        Popularity = popularity;
+        Awards = awards;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public int getLength() {
+        return Length;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public int getPopularity() {
+        return Popularity;
+    }
+
+    public boolean isAwards() {
+        return Awards;
+    }
+}
